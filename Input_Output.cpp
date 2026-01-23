f844c8011bb34acfe18e2ca7f776af93f4a15e45
Shivraj Singh karki <65079961+singh-shivraj@users.noreply.github.com>
2026-01-23 15:40:13 +0530

    Update Input_Output.cpp

--- Input_Output.cpp
+++ Input_Output.cpp
@@ -4,8 +4,19 @@ using namespace std;
 int main()
 {
     int num1,num2,abc;
-std::cout<<"enter the input1:";
-std::cin>>abc;
+std::cout<<"enter the pattern, you want to print:";
+std::cin>>count;
+
+for (size_t i = 0; i < count; i++)
+{
+    for (size_t j = 0; i < count; i++)
+    {
+        cout<<"*";
+    }
+    cout<<"\n";
+    
+}
+
 std::cout<<"\n enter the input2:\n";
 std::cin>>num2;
 
