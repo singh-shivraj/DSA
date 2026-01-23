99d44ec244491cc7873b76d239cadd45d54f7cf6
Shivraj Singh karki <65079961+singh-shivraj@users.noreply.github.com>
2026-01-23 11:21:26 +0530

    commit

---
+++ Input_Output.cpp
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<iostream>
+using namespace std;
+int main()
+{
+    int num1,num2,abc;
+std::cout<<"enter the input1:";
+std::cin>>abc;
+std::cout<<"\n enter the input2:\n";
+std::cin>>num2;
+
+int sum=num1+num2;
+
+std::cout<<"SUM:" <<sum;
+
+}
