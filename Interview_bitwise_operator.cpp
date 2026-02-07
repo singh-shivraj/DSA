37ddb01e1cea68c89cdb42a6105070da9949db8e
Shivraj Singh karki <65079961+singh-shivraj@users.noreply.github.com>
2026-02-07 23:22:00 +0530

    commit

--- Interview_bitwise_operator.cpp
+++ Interview_bitwise_operator.cpp
@@ -3,4 +3,4 @@
 //2.. n= n>>1 -> it will divide n/2
 // log10(n) +1  -->Count the number of 
 // (n & n-1) ->will always give zero if n is even. i.e -> ((n&(n-1)) ==0) ->true if n is zero,else false.(Check power of two solutin)
-//x^0 =x, x^x= 0, x^y = y^x,  
+//x^0 =x, x^x= 0, x^y = y^x,
